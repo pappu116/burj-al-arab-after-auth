4d71bde908a72956d272f00431973b46c850990a
pappu116 <52205941+pappu116@users.noreply.github.com>
2020-09-29 12:49:49 +0600

    connected to server

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBl7XHSqSAWkzrGT48wsaV8eCB3fO8ZFDY",
+  authDomain: "burj-al-arab-8264f.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-8264f.firebaseio.com",
+  projectId: "burj-al-arab-8264f",
+  storageBucket: "burj-al-arab-8264f.appspot.com",
+  messagingSenderId: "514034506148",
+  appId: "1:514034506148:web:429b9e8a85825457980931",
+};
+export default firebaseConfig;
